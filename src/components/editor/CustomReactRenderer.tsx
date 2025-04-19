54e25b457c32b766984062968acc33cbcb45d0f1
Fauzan Rifqy <fauzan@teqplay.com>
2025-04-19 18:44:09 +0700

    Update CustomReactRenderer to use createRoot for React 18 compatibility

--- src/components/editor/CustomReactRenderer.tsx
+++ src/components/editor/CustomReactRenderer.tsx
@@ -1,33 +1,36 @@
 import React from 'react';
-import { createPortal, render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 export class CustomReactRenderer {
   private reactElement: React.ReactElement;
   private domElement: HTMLElement;
   private contentElement: HTMLElement;
+  private root: ReturnType<typeof createRoot>;
 
   constructor(
-    component: React.ComponentType<any>,
-    props: Record<string, any>
+    component: React.ComponentType<Record<string, unknown>>,
+    props: Record<string, unknown>
   ) {
     this.contentElement = document.createElement('div');
     this.domElement = document.createElement('div');
     this.domElement.appendChild(this.contentElement);
     this.reactElement = React.createElement(component, props);
+    this.root = createRoot(this.contentElement);
     this.render();
   }
 
-  updateProps(props: Record<string, any>) {
+  updateProps(props: Record<string, unknown>) {
     this.reactElement = React.createElement(this.reactElement.type, props);
     this.render();
   }
 
   render() {
-    render(this.reactElement, this.contentElement);
+    this.root.render(this.reactElement);
   }
 
   destroy() {
     if (this.domElement.parentNode) {
+      this.root.unmount();
       this.domElement.parentNode.removeChild(this.domElement);
     }
   }
